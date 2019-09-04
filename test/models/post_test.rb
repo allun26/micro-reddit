9cab55113c55acc44a93d24a51ea50c488739a4e
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-04 19:35:23 +0100

    Correct post model to have a length validation on body instead of title. Correct tests for posts to test for the body length instead of the title length

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,17 +4,22 @@ class PostTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Jacob", email: "jacob@yahoo.com")
     @user.save!
-    @post = Post.new(title: "A new post that has a 50 character title for sure, or am I?", body: "Some text", user_id: @user.id)
+    @post = Post.new(title: "A mundane title", body: "A new post that has a 50 character body for sure, or am I?", user_id: @user.id)
   end
 
   test "post should be valid" do
     assert @post.valid?
   end 
 
-  test "post should have a title of at least 50 characters" do
+  test "post should have a title" do
     post_duplicate = @post.dup
-    post_duplicate.title = '2'
+    post_duplicate.title = ''
     assert_not post_duplicate.valid?
+  end
+
+  test "post body should have at least 50 characters" do 
+    post_duplicate = @post.dup
+    post_duplicate.body = "This is definitely not up to 50 characters"
   end
 
   test "post should have a body" do
